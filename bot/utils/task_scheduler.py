fd643589210dda35d9a177f203f992d2a0786c48
ChestnutCMD <126091389+ChestnutCMD@users.noreply.github.com>
2024-03-26 22:32:17 +0500

    Update task_scheduler.py

--- bot/utils/task_scheduler.py
+++ bot/utils/task_scheduler.py
@@ -3,7 +3,7 @@ from database.requests import update_tokens
 
 
 def daily_token_update():
-    schedule.every().day.at('00:01').do(update_tokens)
+    schedule.every().day.at('21:01').do(update_tokens)
     while True:
         schedule.run_pending()
 
